15a2bca083a0423868a0bbcad9a6be91f9be1230
Adamilton <adapereira54@gmail.com>
2021-11-19 03:02:13 -0300

    adicionando javascript

---
+++ HTML/Ex033/js/myscript.js
@@ -0,0 +1,40 @@
+            const myName = document.getElementById("nome");
+            const myState = document.getElementById("estado");
+            const myphoto = document.getElementById("foto");
+            const myAge = document.getElementById("idade");
+            const myTitle = document.getElementById("titulo");
+            const myIntroduction = document.getElementById("introducao");
+            const mainContent = document.getElementById("conteudoPrincipal");
+            const title_1 = document.getElementById("titulo_1");
+            const title_2 = document.getElementById("titulo_2");
+            const title_3 = document.getElementById("titulo_3");
+            const title_4 = document.getElementById("titulo_4");
+            const content_1 = document.getElementById("conteudo_1");
+            const content_2 = document.getElementById("conteudo_2");
+            const content_3 = document.getElementById("conteudo_3");
+            const content_4 = document.getElementById("conteudo_4");
+            const content = document.getElementById("conteudo");
+
+
+
+            myName.innerHTML = "Adamilton Pereira";
+            myState.innerHTML ="Amazonas";
+            document.getElementById("foto").src="imagens/perfil.jpg";
+            myAge.innerHTML ="31 Anos";
+            myTitle.innerHTML="Sem hobby?"
+            myIntroduction.innerHTML="Não tenho hobby, gosto muito de ver filmes, séries e animes. Meu hobby é entretenimento, pode ser um jogo de futebol ou joginho online, gosto muito de aprender algo novo e sinto vivo, gosto de tudo um pouco, além de experimentar e descobrindo as coisas e conversar com pessoas sendo elas parecidas comigo ou não, que tem a mesmo ideias de pensamentos ou não, mas acho que prefiro que não tenha, assim que fica bem mais divertido. ";
+
+           title_2.innerHTML="Filmes";
+           document.getElementById("filme").src="imagens/filme.jpg";
+           content_2.innerHTML="Acho que não foi o meu primeiro contato com entretenimento visual, mas era muito bom reunir a família para ver um  filme e nem precisava ser bom, na verdade, se for besta é melhor, ninguém precisa pensar. Hoje eu entendo que o gosto pelos filmes é como uma tradição antiga que gostaria de reviver.";
+
+           title_3.innerHTML="Séries";
+           document.getElementById("serie").src="imagens/series.jpg";
+           content_3.innerHTML="Quando os filmes, mas não só um filme passa um sentimento que ainda precisa de mais para ser completo ou se algo é tão bom que não queremos que acabe, e assim nascem as séries. É um jeito para esticar uma história e prender o público. Eu não sou diferente, gosto das séries, pois posso aproveitar mais da história e da experiência, mas hoje me dia prefiro as que têm uma carga emocional mais densa, não as outras não são boas, mas isso é só uma fase. "
+            
+           title_4.innerHTML="Animes";
+           document.getElementById("anime").src="imagens/anime.jpg";
+           content_4.innerHTML="Foi um mundo que cedo, na tv aberta poucos canais passavam animes e naquela época não liga como se chamava, pra mim, era só desenho animando e que de longe é uma das coisas que mais me chamava atenção quando era criança. Perdi o contato ao logo do tempo, mas quando me reencontrei com essa arte não abandonei mais. Parece que os criadores não querem produzir somente entretenimento, querem passar um aprendizado, por essa razão gosto tanto de animes, posso viver uma historia e aprender com ela."
+
+           content.innerHTML="© 2021 Adamilton Pereira Melo Junior"
+            
